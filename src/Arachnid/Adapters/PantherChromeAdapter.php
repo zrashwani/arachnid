e3f0a0e43e2601dece69efba5ddae66dc47ba7b3
Antonis Flangofas <antonisflags@gmail.com>
2021-10-14 15:50:04 +0300

    Pass options to chrome client

--- src/Arachnid/Adapters/PantherChromeAdapter.php
+++ src/Arachnid/Adapters/PantherChromeAdapter.php
@@ -8,12 +8,12 @@ use Symfony\Component\Panther\Client as PantherClient;
 class PantherChromeAdapter implements CrawlingAdapterInterface
 {
     private $client;
-    
-    public function __construct()
+
+    public function __construct(array $options)
     {
-        $this->client = PantherClient::createChromeClient();
+        $this->client = PantherClient::createChromeClient(null, null, $options);
     }
-    
+
     public function requestPage($url): Crawler
     {
         return $this->client->request('GET', $url);
